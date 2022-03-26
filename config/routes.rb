66be5ecb901871f2c903b25ec5bac0b6ca50de07
RogelioCamargo <rogelio.camargo2016@gmail.com>
2022-03-26 14:47:04 -0700

    Create User table and model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+	resource :session, only: [:new, :create, :destroy]
+  resources :users, only: [:new, :create]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
