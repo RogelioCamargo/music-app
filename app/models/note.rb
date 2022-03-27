3945bfffe91ebf4d4d58c49cf337131e5138f8ef
RogelioCamargo <rogelio.camargo2016@gmail.com>
2022-03-27 16:11:23 -0700

    Create Note table and model

---
+++ app/models/note.rb
@@ -0,0 +1,6 @@
+class Note < ApplicationRecord
+	validates :content, presence: true 
+	
+	belongs_to :track
+	belongs_to :user 
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_27_192237) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_27_220304) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -29,6 +29,16 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_27_192237) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["name"], name: "index_bands_on_name", unique: true
+  end
+
+  create_table "notes", force: :cascade do |t|
+    t.integer "track_id", null: false
+    t.integer "user_id", null: false
+    t.text "content", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["track_id"], name: "index_notes_on_track_id"
+    t.index ["user_id"], name: "index_notes_on_user_id"
   end
 
   create_table "tracks", force: :cascade do |t|
