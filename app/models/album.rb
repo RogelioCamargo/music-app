fc780be9127a5da6eb7847899d2f2697db1c28f8
RogelioCamargo <rogelio.camargo2016@gmail.com>
2022-03-26 21:06:09 -0700

    Create Album model and estalish associations

---
+++ app/models/album.rb
@@ -0,0 +1,15 @@
+class Album < ApplicationRecord
+	validates :title, :year, presence: true
+	validates :title, uniqueness: { scope: :band_id } 
+	validates :live, inclusion: [true, false]
+	validates :year, numericality: { minimum: 1900, maximum: 9000 }
+	after_initialize :set_defaults 
+
+	belongs_to :band
+
+	private 
+
+	def set_defaults
+		self.live ||= false
+	end
+end
