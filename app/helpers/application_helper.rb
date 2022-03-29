d9e23854bd0a12fe97aa5cd93c9de397a5fa135e
RogelioCamargo <rogelio.camargo2016@gmail.com>
2022-03-29 08:42:49 -0700

    Fix authenticity token method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,10 @@
 module ApplicationHelper
 	def authenticity_token
-		`<input
-			type="hidden"
-			name="authenticity_token"
-			value="#{form_authenticity_token}"
-		>`.html_safe
+		"<input
+			type='hidden'
+			name='authenticity_token'
+			value='#{form_authenticity_token}'
+		>".html_safe
 	end
 
 	def ugly_lyrics(lyrics)
